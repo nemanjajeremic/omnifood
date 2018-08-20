6d4636bacd81f1c4abaabdd77f6f726adfece8fc
Nemanja Jeremic <jnemanja@hotmail.com>
2018-08-20 21:04:00 +0200

    1.21
    Starting with Jquery
    sticky navigation completed

---
+++ resources/js/script.js
@@ -0,0 +1,27 @@
+/*global $ */
+/*global document */
+
+$(document).ready(function() {
+    
+   $('.js--section-features').waypoint(function(direction){
+            if(direction == "down") {
+       $('nav').addClass('sticky');
+   } else {
+       $('nav').removeClass('sticky');
+   }                     
+                                        
+    
+}, {
+  offset: '10%'
+});
+
+/*
+
+var waypoints = $('#handler-first').waypoint(function(direction) {
+  notify(this.element.id + ' hit 25% from top of window') 
+}, {
+  offset: '25%'
+})
+
+*/
+});

d8c4762c17bf5ef17e77b37e464c9745ba2013bd
Nemanja Jeremic <jnemanja@hotmail.com>
2018-08-26 12:25:28 +0200

    1.24
    Responsive menu and jQuery completed, navigation toggle etc.

--- resources/js/script.js
+++ resources/js/script.js
@@ -97,7 +97,22 @@ $('a[href*="#"]')
     
     
     
-    
+    /* MOBILE NAV */
+    $('.js--nav-icon').click(function(){
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+        
+        if (icon.hasClass('ion-ios-menu')) {
+            icon.addClass('ion-ios-close');
+            icon.removeClass('ion-ios-menu');
+        } else {
+             icon.addClass('ion-ios-menu');
+            icon.removeClass('ion-ios-close');
+        }
+        
+        nav.slideToggle(200);
+        
+    });
     
     
     

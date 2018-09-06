adbc010f293635353330bd92e71892eb0c8ddc37
Nemanja Jeremic <jnemanja@hotmail.com>
2018-09-06 21:20:40 +0200

    1.28
    Added Gmaps jquery plugin

--- resources/js/script.js
+++ resources/js/script.js
@@ -22,100 +22,108 @@ $(document).ready(function () {
     $('.js--scroll-to-plans').click(function () {
         $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);                  
     });
-     $('.js--scroll-to-start').click(function () {
+    $('.js--scroll-to-start').click(function () {
         $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000);                  
     });
 
-      /*   NAVIGATION SCROLL */
-    
+    /*   NAVIGATION SCROLL */
+
     // Select all links with hashes
-$('a[href*="#"]')
-  // Remove links that don't actually link to anything
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
-    // On-page links
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-      && 
-      location.hostname == this.hostname
-    ) {
-      // Figure out element to scroll to
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      // Does a scroll target exist?
-      if (target.length) {
-        // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000, function() {
-          // Callback after animation
-          // Must change focus!
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) { // Checking if the target was focused
-            return false;
-          } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            $target.focus(); // Set focus again
-          }
-        });
-      }
-    }
-  });
-    
- /*   ANIMATION ON  SCROLL */
-    
-    
+    $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function(event) {
+        // On-page links
+        if (
+            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+            && 
+            location.hostname == this.hostname
+        ) {
+            // Figure out element to scroll to
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            // Does a scroll target exist?
+            if (target.length) {
+                // Only prevent default if animation is actually gonna happen
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000, function() {
+                    // Callback after animation
+                    // Must change focus!
+                    var $target = $(target);
+                    $target.focus();
+                    if ($target.is(":focus")) { // Checking if the target was focused
+                        return false;
+                    } else {
+                        $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+                        $target.focus(); // Set focus again
+                    }
+                });
+            }
+        }
+    });
+
+    /*   ANIMATION ON  SCROLL */
+
+
     $('.js--wp-1').waypoint(function(direction){
-       $('.js--wp-1').addClass('animated fadeIn');
+        $('.js--wp-1').addClass('animated fadeIn');
     }, {
         offset: "50%"
     });
-    
-    
+
+
     $('.js--wp-2').waypoint(function(direction){
-       $('.js--wp-2').addClass('animated fadeInUp');
+        $('.js--wp-2').addClass('animated fadeInUp');
     }, {
         offset: "50%"
     });
-    
-    
+
+
     $('.js--wp-3').waypoint(function(direction){
-       $('.js--wp-3').addClass('animated fadeIn');
+        $('.js--wp-3').addClass('animated fadeIn');
     }, {
         offset: "50%"
     });
-    
-    
+
+
     $('.js--wp-4').waypoint(function(direction){
-       $('.js--wp-4').addClass('animated pulse');
+        $('.js--wp-4').addClass('animated pulse');
     }, {
         offset: "50%"
     });
-    
-    
-    
+
+
+
     /* MOBILE NAV */
     $('.js--nav-icon').click(function(){
         var nav = $('.js--main-nav');
         var icon = $('.js--nav-icon i');
-        
+
         if (icon.hasClass('ion-ios-menu')) {
             icon.addClass('ion-ios-close');
             icon.removeClass('ion-ios-menu');
         } else {
-             icon.addClass('ion-ios-menu');
+            icon.addClass('ion-ios-menu');
             icon.removeClass('ion-ios-close');
         }
-        
+
         nav.slideToggle(200);
-        
+
     });
-    
-    
-    
-    
-    
+
+
+    /* MAPS */
+
+    new GMaps({
+        div: '.map',
+        lat: 44.8154033,
+        lng: 20.2825125,
+        zoom: 12
+    });
+
+
+
 });
